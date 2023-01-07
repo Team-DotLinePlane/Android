ea7e82452a37761f15d5faca3e83d18885faff8a
dlgocks1 <cksgodl1@naver.com>
2023-01-07 21:59:08 +0900

    feat : mapFragment 작업 완료

--- app/src/main/java/com/example/dlp/ui/map/adapter/MapCategoryAdapter.kt
+++ app/src/main/java/com/example/dlp/ui/map/adapter/MapCategoryAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.dlp.ui.map.adapter
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
@@ -10,6 +11,8 @@ import com.example.dlp.databinding.ItemMapCategoryBinding
 class MapCategoryAdapter :
     ListAdapter<String, MapCategoryAdapter.ViewHolder>(PlanDiffCallback) {
 
+    var selectedStr = DEFAULT_SELECTED
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
             ItemMapCategoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -17,21 +20,22 @@ class MapCategoryAdapter :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val plan = currentList[position]
-        holder.bind(plan)
+        val searchStr = currentList[position]
+        holder.bind(searchStr)
         holder.itemView.setOnClickListener {
             if (onItemClickListener != null) {
-                onItemClickListener?.invoke(plan)
+                onItemClickListener?.invoke(searchStr)
             }
         }
     }
 
     private var onItemClickListener: ((String) -> Unit)? = null
-    fun setOnItemClickListener(action: (String) -> Unit) {
-        onItemClickListener = action
+    fun setOnItemClickListener(searchStr: (String) -> Unit) {
+        onItemClickListener = searchStr
     }
 
     companion object {
+        const val DEFAULT_SELECTED = "한식"
         private val PlanDiffCallback = object : DiffUtil.ItemCallback<String>() {
             override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
                 return oldItem == newItem
@@ -48,6 +52,12 @@ class MapCategoryAdapter :
     ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(str: String) {
             binding.searchTv.text = str
+            if (selectedStr == str) {
+                binding.searchTv.setTextColor(Color.parseColor("#3360FF"))
+            } else {
+                binding.searchTv.setTextColor(Color.parseColor("#AFAFAF"))
+            }
         }
     }
+
 }
