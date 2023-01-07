0bc444e3d31fc4c1c536bfa267b11df4486c40d5
seunggyu <seunggyu97@gmail.com>
2023-01-07 20:28:35 +0900

    [Feature] homefragment

--- app/src/main/java/com/example/dlp/CreateGroupFragment.kt
+++ app/src/main/java/com/example/dlp/CreateGroupFragment.kt
@@ -6,16 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.dlp.databinding.FragCreategroupBinding
 import com.example.dlp.databinding.FragHomeBinding
 
-class HomeFragment : Fragment() {
-    lateinit var binding : FragHomeBinding
+class CreateGroupFragment : Fragment() {
+    lateinit var binding : FragCreategroupBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater, R.layout.frag_home, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.frag_creategroup, container, false)
         return binding.root
     }
 }
